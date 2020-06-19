ad6ed022005f2c11f7a808f0a33f620087b21ac2
Derek Wright <git@dwwright.net>
2020-06-18 15:31:37 -1000

    Issue #3153005 by dww: Simplify src/Field/EntityGroupFieldItemList.php

--- src/Field/EntityGroupFieldItemList.php
+++ src/Field/EntityGroupFieldItemList.php
@@ -6,7 +6,6 @@ use Drupal\Core\Field\EntityReferenceFieldItemList;
 use Drupal\Core\TypedData\ComputedItemListTrait;
 use Drupal\Core\TypedData\DataDefinitionInterface;
 use Drupal\Core\TypedData\TypedDataInterface;
-use Drupal\Core\DependencyInjection\DependencySerializationTrait;
 use Drupal\group\Entity\GroupContent;
 
 /**
@@ -14,44 +13,20 @@ use Drupal\group\Entity\GroupContent;
  */
 class EntityGroupFieldItemList extends EntityReferenceFieldItemList {
 
-  // Support non-database views. Ex: Search API Solr.
-  use DependencySerializationTrait;
   use ComputedItemListTrait;
-
-  /**
-   * The Group Content Plugin Manager.
-   *
-   * @var \Drupal\group\Plugin\GroupContentEnablerManagerInterface
-   */
-  protected $groupContentPluginManager;
-
-  /**
-   * Constructs a EntityGroupFieldItemList object.
-   *
-   * @param \Drupal\Core\TypedData\DataDefinitionInterface $definition
-   *   The data definition.
-   * @param string $name
-   *   (optional) The name of the created property, or NULL if it is the root
-   *   of a typed data tree. Defaults to NULL.
-   * @param \Drupal\Core\TypedData\TypedDataInterface $parent
-   *   (optional) The parent object of the data property, or NULL if it is the
-   *   root of a typed data tree. Defaults to NULL.
-   */
-  public function __construct(DataDefinitionInterface $definition, $name = NULL, TypedDataInterface $parent = NULL) {
-    parent::__construct($definition, $name, $parent);
-    $this->groupContentPluginManager = \Drupal::service('plugin.manager.group_content_enabler');
-  }
 
   /**
    * {@inheritdoc}
    */
   public function computeValue() {
-    $plugin_id = $this->groupContentPluginManager->getPluginIdByEntityType($this->getEntity()->getEntityTypeId(), $this->getEntity()->bundle());
-    if (!$plugin_id) {
+    // No value will exist if the entity has not been created, so exit early.
+    if ($this->getEntity()->isNew()) {
       return NULL;
     }
-    // No value will exist if the entity has not been created so exit early.
-    if ($this->getEntity()->isNew()) {
+
+    // If this entity/bundle has no group content enabler plugins enabled,
+    // there's no way there could be any group associations, so exit early.
+    if (!\Drupal::service('plugin.manager.group_content_enabler')->getPluginIdsByEntityType($this->getEntity()->getEntityTypeId(), $this->getEntity()->bundle())) {
       return NULL;
     }
 
@@ -61,12 +36,10 @@ class EntityGroupFieldItemList extends EntityReferenceFieldItemList {
     }
 
     $this->list = [];
-    if (!empty($group_contents)) {
-      foreach ($group_contents as $delta => $group_content) {
-        $this->list[] = $this->createItem($delta, [
-          'target_id' => $group_content->id(),
-        ]);
-      }
+    foreach ($group_contents as $delta => $group_content) {
+      $this->list[] = $this->createItem($delta, [
+        'target_id' => $group_content->id(),
+      ]);
     }
   }
 
