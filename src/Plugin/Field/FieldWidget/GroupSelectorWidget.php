41a0487c00d8360cf33c4306aa0a4b78edc92aea
Derek Wright <git@dwwright.net>
2020-06-24 13:50:13 -1000

    Issue #3153401 by dww: Put a deprecated plugin at the old name.

---
+++ src/Plugin/Field/FieldWidget/GroupSelectorWidget.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Drupal\entitygroupfield\Plugin\Field\FieldWidget;
+
+/**
+ * Plugin implementation of the deprecated 'group_selector_widget' widget.
+ *
+ * @deprecated in entitygroupfield:1.0.0 and is removed from
+ *   entitygroupfield:2.0.0. Use either 'entitygroupfield_select_widget' or
+ *   'entitygroupfield_autocomplete_widget' instead.
+ *
+ * @FieldWidget(
+ *   id = "group_selector_widget",
+ *   label = @Translation("(Deprecated) Group selector"),
+ *   field_types = {
+ *     "group_content"
+ *   }
+ * )
+ */
+class GroupSelectorWidget extends EntityGroupFieldSelectWidget {
+
+}
