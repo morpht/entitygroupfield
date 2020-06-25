a7c7514b660f4eba64dfadafb154308ac71394ab
Derek Wright <git@dwwright.net>
2020-06-25 11:53:11 -1000

    Issue #3153401 by dww: Fix default_widget and default_formatter annotations to use new plugin IDs.

--- src/Plugin/Field/FieldType/GroupContentItem.php
+++ src/Plugin/Field/FieldType/GroupContentItem.php
@@ -11,8 +11,8 @@ use Drupal\Core\Field\Plugin\Field\FieldType\EntityReferenceItem;
  *   id = "group_content",
  *   label = @Translation("Groups"),
  *   description = @Translation("This is a computed field to relate content with groups"),
- *   default_widget = "group_selector_widget",
- *   default_formatter = "parent_group_entity_formatter",
+ *   default_widget = "entitygroupfield_select_widget",
+ *   default_formatter = "parent_group_label_formatter",
  *   list_class = "\Drupal\entitygroupfield\Field\EntityGroupFieldItemList",
  * )
  */
