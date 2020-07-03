5fe22cd15839265d022c1d0e10b32e3fca3fd78e
Derek Wright <git@dwwright.net>
2020-07-02 20:28:13 -1000

    Issue #3153401 by dww: Add @see and fix deprecation versions for legacy widget

--- src/Plugin/Field/FieldWidget/GroupSelectorWidget.php
+++ src/Plugin/Field/FieldWidget/GroupSelectorWidget.php
@@ -5,9 +5,10 @@ namespace Drupal\entitygroupfield\Plugin\Field\FieldWidget;
 /**
  * Plugin implementation of the deprecated 'group_selector_widget' widget.
  *
- * @deprecated in entitygroupfield:1.0.0 and is removed from
- *   entitygroupfield:2.0.0. Use either 'entitygroupfield_select_widget' or
+ * @deprecated in entitygroupfield:0.0.0 and is removed from
+ *   entitygroupfield:1.0.0. Use either 'entitygroupfield_select_widget' or
  *   'entitygroupfield_autocomplete_widget' instead.
+ * @see https://www.drupal.org/project/entitygroupfield/issues/3153401
  *
  * @FieldWidget(
  *   id = "group_selector_widget",
